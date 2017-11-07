a12b2041c62ca04a5a5f8557c1e2fa693251981e
chanThen <33303584+chanThen@users.noreply.github.com>
2017-11-07 21:25:11 +0530

    Add files via upload

---
+++ bookWorld/WebContent/scripts/controller/subscribeController.js
@@ -0,0 +1,48 @@
+angular.module('myApp')
+.controller('subscribeCtrl', [
+	'$scope',
+	'$http','$state','subscribeService', function (
+			$scope,
+			$http,$state,
+			subscribeService) {
+				$scope.bookId = $state.params.bookid;
+				var bookid = $scope.bookId ;
+				
+				
+				$scope.payToSubscribe = function payToSubscribe() {
+						/*$scope.username;
+						$scope.phonenumber;
+						$scope.accountnumber;
+						$scope.cardtype;*/
+						$scope.bookId = JSON.parse($state.params.bookid);
+						var user = sessionStorage.getItem('userData');
+						var userData = JSON.parse(user);
+
+						subscribeService.payToSubscribe(userData.userid,bookid,callback);
+				};
+
+		function callback(payload){
+					$state.go('root.dashBoard.singleProduct',{bookid:bookid});
+					/*bootbox.confirm("Cick to Confirm? ", function(result) {
+					if(result == true){*//*	}				
+			});*/}
+
+		var init = function init() {
+			  var user = sessionStorage.getItem('userData');
+			  var userData = JSON.parse(user);
+			  subscribeService.getAllSubscribedBooksByUserId(userData.userid,function(response){
+				  $scope.subscribedData = response;
+				  console.log($scope.subscribedData);
+			 });					   
+		};
+		
+		$scope.viewDetails = function viewDetails(bookId) {
+			$state.go('root.dashBoard.singleProduct',{bookid:bookId});
+		}
+		
+		init();
+	}]);
+
+
+
+
